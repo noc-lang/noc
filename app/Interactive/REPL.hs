8802e22664693a70b669905bd93f81c5b4eeceb4
kaoulad <kaoulad@protonmail.com>
2021-01-02 13:32:43 +0100

    🔥 Removing code

--- app/Interactive/REPL.hs
+++ app/Interactive/REPL.hs
@@ -1,7 +1,7 @@
 module Interactive.REPL where
 
 import Language.Noc.Runtime.Eval 
-import Language.Noc.Runtime.Internal (Values)
+import Language.Noc.Runtime.Internal
 import Language.Noc.Syntax.AST (parseNoc,REPLProgram)
 import Interactive.Commands
 import System.IO (hFlush, stdout)
@@ -9,7 +9,7 @@ import System.IO (hFlush, stdout)
 type Env = [String]
 
 --------------- Utils ----------------------------------
-nocREPL :: Values -> Env -> IO ()
+nocREPL :: String -> Env -> IO ()
 nocREPL stack env = prompt >>= (repl stack env)
 
 -- flushing stdout buffer before calling getLine
@@ -22,13 +22,13 @@ cmd name funcs = action
                 where (Just f) = lookup name funcs
                       (REPLCommands _ _ action) = f
 
-run :: String -> [(String, REPLCommands)] -> Values -> Env -> IO ()
+run :: String -> [(String, REPLCommands)] -> String -> Env -> IO ()
 run name funcs stack env = case name `elem` (map fst funcs) of
                             True  -> cmd name funcs
                             False -> putStrLn ("Unknown command '" ++ name ++ "' or lack of arguments")
 
 ----------------- REPL function ---------------------------
-repl :: Values -> Env -> [String] -> IO ()
+repl :: String -> Env -> [String] -> IO ()
 repl stack env [] = nocREPL stack env
 repl stack env  [(':' : cmd)] = (run cmd singleCommands stack env) >> nocREPL stack env
 repl stack env ((':':cmd):args) = (run cmd (commandsArgs args) stack env) >> nocREPL stack env
