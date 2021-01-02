535ae84e938940ca3bbcde7f6075544942a5b414
kaoulad <kaoulad@protonmail.com>
2021-01-02 13:02:51 +0100

    🎨 Improve format of the code

--- app/Main.hs
+++ app/Main.hs
@@ -3,5 +3,8 @@ module Main where
 import Interpreter.CLI (opts, run)
 import Options.Applicative (execParser)
 
+import Language.Noc.Syntax.AST (parseNoc)
+import Language.Noc.Runtime.Eval (eval)
+
 main :: IO ()
 main = run =<< (execParser opts)
--- src/Language/Noc/Runtime/Internal.hs
+++ src/Language/Noc/Runtime/Internal.hs
@@ -1,19 +1,4 @@
 module Language.Noc.Runtime.Internal where
 
 ----------------------- Modules --------------------------------------------------
-import Language.Noc.Syntax.AST (Expr, REPLInput(DeclInput,ExprInput))
-import Data.HashMap
-import Control.Monad.Error (MonadError)
-import Control.Monad.Except (Except)
-import Control.Monad.RWS (RWST,MonadRWS)
-import qualified Data.Map as Map
-
--- Datatype exceptions
---data EvalError = ZeroDivisionError | NegativeLogError | EmptyStackError deriving Show
---type Env = Map String Expr
-
---newtype Eval a = Eval { unEval :: RWST Env () Expr (Except EvalError) a } deriving (Functor, Applicative, Monad, MonadRWS Env () Expr, MonadError EvalError)
-
-data Value = FloatVal Double | StringVal String deriving Show
-type Values = [Value]
-
+import Language.Noc.Syntax.AST
